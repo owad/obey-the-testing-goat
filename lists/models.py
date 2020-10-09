5955826c97a11d3e82e15282bc8c429c41cd2478
Łukasz Lechowicz <lukasz.lechowicz@made.com>
2020-10-09 09:45:39 +0200

    add the List model

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
-class Item(models.Model):
-    text = models.CharField(max_length=255, default="")
+class List(models.Model):
+    pass
 
+
+class Item(models.Model):
+    text = models.TextField(default="")
+    list = models.ForeignKey(List, default=None)
